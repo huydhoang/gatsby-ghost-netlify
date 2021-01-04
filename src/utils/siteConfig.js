2d5104f7e59f9db10443ef89581f5f6d8f63b603
Huy <8493044+huydhoang@users.noreply.github.com>
2021-01-04 21:34:33 +0700

    Update siteConfig.js

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -3,7 +3,7 @@ module.exports = {
 
     postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
-    siteTitleMeta: `My life, my stories.`, // This allows an alternative site title for meta data for pages.
+    siteTitleMeta: `Making sense of my life`, // This allows an alternative site title for meta data for pages.
     siteDescriptionMeta: `Thoughts, notes and stories of all the struggles I had`, // This allows an alternative site description for meta data for pages.
 
     shareImageWidth: 1000, // Change to the width of your default share image
